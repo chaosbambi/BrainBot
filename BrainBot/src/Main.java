28c8faa8f8b50b222cb594832d8dded957c1fbb0
Shad0wlife <matze_skywalker@hotmail.de>
2018-04-21 00:42:33 +0200

    Fixed Bot launch

--- BrainBot/src/Main.java
+++ BrainBot/src/Main.java
@@ -5,17 +5,19 @@ import org.telegram.telegrambots.exceptions.TelegramApiRequestException;
 public class Main {
 
 	public static void main(String[] args) {
-		TestBot tst = new TestBot();
+		System.out.println("Starting Bot.");
 		
 		//Initialize Api Context
 		ApiContextInitializer.init();
 
         //Instantiate Telegram Bots API
 		TelegramBotsApi botsApi = new TelegramBotsApi();
-
+		
+		TestBot t = new TestBot();
         //Register our bot
 		try {
-			botsApi.registerBot(tst);
+			botsApi.registerBot(t);
+			System.out.println("Bot registered!");
 		} catch (TelegramApiRequestException e) {
 			e.printStackTrace();
 		}
