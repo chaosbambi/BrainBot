b5b178395a26503a607602b3fa4037eeb4b9b962
Shad0wlife <matze_skywalker@hotmail.de>
2018-04-21 02:15:01 +0200

    Voice Message Saving - Proof of concept
    Saves a received voice message from a generated URL linking to the voice
    message ogg file. Downloading is handled by an apache library.

--- BrainBot/src/TestBot.java
+++ BrainBot/src/TestBot.java
@@ -1,4 +1,13 @@
+//import java.io.File;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.apache.commons.io.FileUtils;
+import org.telegram.telegrambots.api.methods.GetFile;
 import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.File;
 import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
@@ -12,25 +21,67 @@ public class TestBot extends TelegramLongPollingBot{
 
 	@Override
 	public void onUpdateReceived(Update update) {
-		if(update.hasMessage() && update.getMessage().hasText()) {
-			SendMessage sendMsg = new SendMessage().setChatId(update.getMessage().getChatId());
-			if(update.getMessage().getText().toLowerCase().trim().matches("ping[\\.!]*")) {
-				sendMsg.setText("Pong!");
-			}else {
-				sendMsg.setText("Sorry, das habe ich nicht verstanden...");
-			}
-			try {
-				execute(sendMsg);
-			} catch (TelegramApiException e) {
-				e.printStackTrace();
+		if(update.hasMessage()) {
+			if(update.getMessage().hasText()) {
+				receiveText(update);
+			}else if(update.getMessage().getVoice() != null) {
+				saveVoice(update);				
 			}
 		}
-		
 	}
 
 	@Override
 	public String getBotToken() {
 		return Sensitive.getKey();
+	}
+	
+	/**
+	 * Empfängt text aus einer nachricht.
+	 * @param update Das Update-Objekt, was die Verarbeitung ausgelöst hat.
+	 */
+	private void receiveText(Update update) {
+		SendMessage sendMsg = new SendMessage().setChatId(update.getMessage().getChatId());
+		if(update.getMessage().getText().toLowerCase().trim().matches("ping[\\.!]*")) {
+			sendMsg.setText("Pong!");
+		}else {
+			sendMsg.setText("Sorry, das habe ich nicht verstanden...");
+		}
+		try {
+			execute(sendMsg);
+		} catch (TelegramApiException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Speichert eine empfangene Voice Nachricht als ogg datei
+	 * @param update Das Update-Objekt, was die Verarbeitung ausgelöst hat.
+	 */
+	private void saveVoice(Update update) {
+		String fileID = update.getMessage().getVoice().getFileId();
+		GetFile getfile = new GetFile().setFileId(fileID);
+		File tf;
+		String url = null;
+		try {
+			tf = this.execute(getfile);
+			url = tf.getFileUrl(Sensitive.getKey());
+		} catch (TelegramApiException e) {
+			e.printStackTrace();
+		}
+		if(url != null && !url.isEmpty()) {
+			URL link = null;
+			try {
+				link = new URL(url);
+			} catch (MalformedURLException e) {
+				e.printStackTrace();
+			}
+			java.io.File target = new java.io.File("voice/"+fileID+".ogg");
+			try {
+				FileUtils.copyURLToFile(link, target, 1000, 2000);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
