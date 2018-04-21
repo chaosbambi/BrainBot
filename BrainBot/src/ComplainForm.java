2d9676d03b27951ff433a0760d17edf9abaea6ca
MSandeman <msandermann96@gmail.com>
2018-04-21 15:47:00 +0200

    Added class to store data that a user wants to add to the Complainform

---
+++ BrainBot/src/ComplainForm.java
@@ -0,0 +1,66 @@
+
+public class ComplainForm {
+	private String place;
+	private String reason;
+	private String line;
+	private String direction;
+	private String time;
+	private String date;
+	private String station;
+	private String message;
+	
+	public String getPlace() {
+		return place;
+	}
+	public void setPlace(String place) {
+		this.place = place;
+	}
+	public String getReason() {
+		return reason;
+	}
+	public void setReason(String reason) {
+		this.reason = reason;
+	}
+	public String getLine() {
+		return line;
+	}
+	public void setLine(String line) {
+		this.line = line;
+	}
+	public String getDirection() {
+		return direction;
+	}
+	public void setDirection(String direction) {
+		this.direction = direction;
+	}
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getStation() {
+		return station;
+	}
+	public void setStation(String station) {
+		this.station = station;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	public void fillHtmlForm() {
+		//Not implemented yet
+	}
+	
+	
+}
