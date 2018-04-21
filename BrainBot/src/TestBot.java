6d7eea1f45d193975443bc4c84abc3cbbc942038
MSandeman <msandermann96@gmail.com>
2018-04-21 02:29:28 +0200

    Added Test for UserID

--- BrainBot/src/TestBot.java
+++ BrainBot/src/TestBot.java
@@ -1,5 +1,7 @@
 import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Message;
 import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.api.objects.User;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
 
@@ -16,6 +18,10 @@ public class TestBot extends TelegramLongPollingBot{
 			SendMessage sendMsg = new SendMessage().setChatId(update.getMessage().getChatId());
 			if(update.getMessage().getText().toLowerCase().trim().matches("ping[\\.!]*")) {
 				sendMsg.setText("Pong!");
+			}
+			else if(update.getMessage().getText().matches("/start")){
+				startWelcomeDialog(update.getMessage());
+				sendMsg.setText("Welcome process started.");
 			}else {
 				sendMsg.setText("Sorry, das habe ich nicht verstanden...");
 			}
@@ -26,6 +32,14 @@ public class TestBot extends TelegramLongPollingBot{
 			}
 		}
 		
+	}
+
+	/*
+	 * This method gathers the personal information from the User
+	 */
+	private void startWelcomeDialog(Message message) {
+		User maybeAdmin = message.getFrom();
+		System.out.println(maybeAdmin.getId());
 	}
 
 	@Override
